fba191ea7a2d7185b8545de446c4e6af9a9a935a
BrunoCaputo <brunocaputo@gec.inatel.br>
2019-03-21 10:53:34 -0300

    Adicionado repositorios da JPA

---
+++ src/main/java/com/kazale/pontointeligente/repositories/EmpresaRepository.java
@@ -0,0 +1,13 @@
+package com.kazale.pontointeligente.repositories;
+
+import com.kazale.pontointeligente.entities.Empresa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+	
+	@Transactional(readOnly = true)
+	Empresa findByCnpj(String cnpj);
+
+}
